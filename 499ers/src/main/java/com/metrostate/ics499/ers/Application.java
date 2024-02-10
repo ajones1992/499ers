46f378708990a7625e594256788e63923e4e6b2d
Alex Jones <ajones052292@gmail.com>
2024-02-10 13:33:03 -0600

    Adding comment to test commit and push functionality.

--- 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// Katherine commit attempt 3
+		// Alex commit v.1
 		SpringApplication.run(Application.class, args);
 	}
 
