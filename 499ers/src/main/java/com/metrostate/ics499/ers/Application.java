2818b260e3501d93f985ff3526e4e245dffbd2ee
KiyokoNee <k.c.gearing@gmail.com>
2024-02-10 13:25:38 -0600

    Katherine's test commit

--- 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		// Katherine made a successful commit
+		// Katherine commit attempt
 		SpringApplication.run(Application.class, args);
 	}
 
