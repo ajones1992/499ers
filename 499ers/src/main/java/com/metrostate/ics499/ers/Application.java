ed52a12ca8cfd2ea2cf21b2759d80bc5669a2a97
David Pankratz <david.pankratz@outlook.com>
2024-02-10 14:01:17 -0600

    Test Commit

--- 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		// Katherine commit attempt 3
 		// Alex commit v.1
+		// Pankratz Commit
 		SpringApplication.run(Application.class, args);
 	}
 
