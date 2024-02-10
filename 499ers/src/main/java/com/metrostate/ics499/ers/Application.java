1fe67319b6f7e47b27a937bfb9d5e1736cafaec6
Alex Jones <ajones052292@gmail.com>
2024-02-10 13:18:28 -0600

    Initial commit

---
+++ 499ers/src/main/java/com/metrostate/ics499/ers/Application.java
@@ -0,0 +1,13 @@
+package com.metrostate.ics499.ers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
