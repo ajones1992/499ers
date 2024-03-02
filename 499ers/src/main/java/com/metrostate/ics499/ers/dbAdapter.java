abd27ec04d8a07fe3b5d906df67eb7e4425fcdfc
David Pankratz <david.pankratz@outlook.com>
2024-03-02 13:15:23 -0600

    dbAdapter commit
    basic insert, update, delete, and query that take sql string statements
    
    framework for insert, update, and delete for Animal made pending the introduction of getters and setters.
    
    framework for insert of persons and locations

--- 499ers/src/main/java/com/metrostate/ics499/ers/dbAdapter.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/dbAdapter.java
@@ -14,33 +14,31 @@ public class dbAdapter {
     //instance variables
     private Connection connect = null;
     private Statement statement = null;
-    private PreparedStatement preparedStatement = null; // not used, but maybe in the future
+    private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
 
     /**
      * privately constructs and initializes the dbAdapter
      *
-     * @param host the host/client of the database (i.e. localhost)
-     * @param dbName Name of the database
+     * @param url host and database url (i.e. jdbc:sqlite://localhost/myDBdemo.db)
      * @param user valid user of the database
      * @param password password of the valid user to access
      */
-    private dbAdapter(String host, String dbName, String user, String password) {
-        initDatabase(host, dbName, user, password);
+    private dbAdapter(String url, String user, String password) {
+            connect(url, user, password);
     }
 
     /**
-     * Gets a single instance of the dbAdapter.
+     * Gets the instance of the dbAdapter.
      *
-     * @param host the host/client of the database (i.e. localhost)
-     * @param dbName Name of the database
+     * @param url host and database url (i.e. jdbc:sqlite://localhost/myDBdemo.db)
      * @param user valid user of the database
      * @param password password of the valid user to access
      * @return The instance of the dbAdapter
      */
-    public static dbAdapter getInstance(String host, String dbName, String user, String password) {
+    public static dbAdapter getInstance(String url, String user, String password) {
         if (database == null) {
-            database = new dbAdapter(host, dbName, user, password);
+            database = new dbAdapter(url, user, password);
         }
         return database;
     }
@@ -49,28 +47,25 @@ public class dbAdapter {
      * initializes and connects to the database with the specified host and name from a
      * user with a valid password.
      *
-     * @param host the host/client of the database (i.e. localhost)
-     * @param dbName Name of the database
+     * @param url host and database url (i.e. jdbc:sqlite://localhost:3306/myDBdemo)
      * @param user valid user of the database
      * @param password password of the valid user to access
+     * @throws IllegalArgumentException when connection fails due to bad arguments
      */
-    private void initDatabase(String host, String dbName, String user, String password) {
+    private void connect(String url, String user, String password)
+            throws IllegalArgumentException {
         try {
-            // load MySQL Driver (may change in not using mysql)
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // setup connection (may change in not using mysql)
-            connect = DriverManager
-                    .getConnection("jbdc:mysql://" + host + "/" + dbName + "?" + "user=" + user + "&password=" + password);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            connect = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
         }
+
     }
 
     /**
      * Closes the Connection to the database
      *
-     * @throws SQLException throws when internal close methods fail
+     * @throws SQLException throws when any close method executes and fails
      */
     public void close() throws SQLException {
         if (resultSet != null)  resultSet.close();
@@ -84,7 +79,7 @@ public class dbAdapter {
      * arguments for an animal, Person, or Location)
      *
      * @param insertStatement the insert statement in sql
-     * @throws SQLException throws when internal methods fail
+     * @throws SQLException throws when the SQL statement is malformed
      */
     public void insert(String insertStatement) throws SQLException {
         statement = connect.createStatement();
@@ -95,11 +90,12 @@ public class dbAdapter {
      * Inserts the specified animal into the Animal table within the database.
      *
      * @param animal animal to be inserted
-     * @throws SQLException throws when internal methods fail
+     * @throws SQLException throws when the SQL statement is malformed
      */
     public void insert (Animal animal) throws SQLException {
-        // insert int Database.Tablename (i.e. Fragmenent.Player)
-        preparedStatement = connect.prepareStatement("insert into Animal values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        // insert into Database.Tablename (i.e. Fragmenent.Player)
+        preparedStatement = connect
+                .prepareStatement("insert into Animal values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         // add for each attribute for Animal once they have getters
         // preparedStatement.setInt(1, animal.getId());
         // preparedStatement.setString(2, animal.getName());
@@ -111,7 +107,57 @@ public class dbAdapter {
         // preparedStatement.setDate(8, animal.getExitDate());
         // preparedStatement.setString(9, animal.getCode());
         // preparedStatement.setString(10, animal.getNotes());
-        // preparedStatement.setString(11, animal.getRecords()); // might need to be handled differently, not decomposed.
+
+        // might need to be handled differently, not decomposed. iterate through list to insert
+        // into different table
+        // preparedStatement.setString(11, animal.getRecords());
+        preparedStatement.executeUpdate();
+    }
+
+    /**
+     * Inserts the specified person into the person table within the database.
+     * May need to be separated into specific instance of Employees, Adoptees,
+     * and fosters.
+     *
+     * @param person person to be inserted
+     * @throws SQLException throws when the SQL statement is malformed
+     */
+    public void insert (Person person) throws SQLException {
+        // insert into Database.Tablename (i.e. Fragmenent.Player)
+        preparedStatement = connect
+                .prepareStatement("insert into Person values (?, ?, ?, ?, ?)");
+
+        // add for each attribute for Person once they have getters
+        // preparedStatement.setInt(1, person.getId());
+        // preparedStatement.setString(2, person.getName());
+        // preparedStatement.setString(3, person.getPhone());
+        // preparedStatement.setString(4, person.getAddress());
+        // preparedStatement.setString(5, person.getType().toString());
+        preparedStatement.executeUpdate();
+    }
+
+    /**
+     * Inserts the specified Location into the person table within the database.
+     * May need to be separated into specific instances of Shelters and Fosters
+     *
+     * @param location location to be inserted
+     * @throws SQLException throws when the SQL statement is malformed
+     */
+    public void insert (Location location) throws SQLException {
+        // insert into Database.Tablename (i.e. Fragmenent.Player)
+        preparedStatement = connect
+                .prepareStatement("insert into Location values (?, ?, ?, ?, ?)");
+
+        // add for each attribute for Location once they have getters
+        // preparedStatement.setString(1, location.getType().toString());
+        // preparedStatement.setInt(2, location.getId());
+        // preparedStatement.setString(3, location.getName());
+        // preparedStatement.setString(4, location.getAddress());
+        // preparedStatement.setInt(5, location.getMaxCapacity());
+
+        // next are lists and will have to be iterated through
+        // preparedStatement.setString(6, location.getSpecies());
+        // preparedStatement.setString(7, location.getAnimals());
         preparedStatement.executeUpdate();
     }
 
@@ -121,7 +167,7 @@ public class dbAdapter {
      * overloaded to include arguments for an animal, Person, or Location)
      *
      * @param DeleteStatement the delete statement in sql
-     * @throws SQLException throws when internal methods fail
+     * @throws SQLException throws when the SQL statement is malformed
      */
     public void delete(String DeleteStatement) throws SQLException {
         statement = connect.createStatement();
