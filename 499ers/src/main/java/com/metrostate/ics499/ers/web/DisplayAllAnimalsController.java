2a3fcadc7513d6cfd1b3d0ab022a2a3fd97221d4
David Pankratz <david.pankratz@outlook.com>
2024-03-23 00:20:55 -0500

    display all animals web update

---
+++ 499ers/src/main/java/com/metrostate/ics499/ers/web/DisplayAllAnimalsController.java
@@ -0,0 +1,34 @@
+package com.metrostate.ics499.ers.web;
+
+import org.springframework.ui.Model;
+import com.metrostate.ics499.ers.Animal;
+import com.metrostate.ics499.ers.Types;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/displayallanimals")
+public class DisplayAllAnimalsController {
+
+    private static final List<Animal> animals = new ArrayList();
+    static {
+        String[] names = {"Mikleo","Echo","Jack","Solaire","Mia","Sophia"};
+        for(int i=0; i < names.length; i++) {
+            animals.add(new Animal(names[i], Types.SpeciesAvailable.CAT, "Tom",
+                    20 - i, LocalDate.of(2015 + i, 4 + i, 16 + i), LocalDate.now(),
+                    "Note "));
+        }
+    }
+
+    @GetMapping
+    public String getAllAnimals(Model model) {
+        model.addAttribute("animals", animals);
+        return "displayallanimals";
+    }
+
+}
