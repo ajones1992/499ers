753eacc6135ebcc09a5a507a8bbdde731827923f
KiyokoNee <k.c.gearing@gmail.com>
2024-03-23 09:38:36 -0500

    just adding what i got for now. some of this probably can be deleted and consolidated

---
+++ 499ers/src/main/java/com/metrostate/ics499/ers/web/UpdateLocationController.java
@@ -0,0 +1,14 @@
+package com.metrostate.ics499.ers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UpdateLocationController {
+
+    @RequestMapping(value = "/updatelocdata", method = RequestMethod.GET)
+    public String updateLocation() {
+        return "updatelocdata";
+    }
+}
