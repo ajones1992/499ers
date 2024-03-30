7500fb0ec48d462c585ab0c0a35078376a012a0f
KiyokoNee <k.c.gearing@gmail.com>
2024-03-30 00:43:08 -0500

    attempted modifications to addanimal. no way to test at present.

--- 499ers/src/main/java/com/metrostate/ics499/ers/web/AddAnimalController.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/web/AddAnimalController.java
@@ -1,19 +1,25 @@
 package com.metrostate.ics499.ers.web;
 
+import com.metrostate.ics499.ers.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/addanimal")
+
 public class AddAnimalController {
 
-    public AddAnimalController() {
-
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/addanimal", method = RequestMethod.GET)
     public String getAnimal() {
         return "addanimal";
+    }
+
+    @RequestMapping(path = "/dataaddedsuccess", method = RequestMethod.POST)
+    public String contactFormSubmission(@ModelAttribute final Animal newAnimal){
+
+        return "redirect:success.html";
     }
 }
