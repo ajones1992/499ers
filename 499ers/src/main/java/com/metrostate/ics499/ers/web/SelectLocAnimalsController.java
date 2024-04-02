b95de16a1dc12d94d4f006be53dcbd0141faf06a
Alex Jones <ajones052292@gmail.com>
2024-04-02 14:48:40 -0500

    Drop down population completed, but submit button is broken still.

--- 499ers/src/main/java/com/metrostate/ics499/ers/web/SelectLocAnimalsController.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/web/SelectLocAnimalsController.java
@@ -1,17 +1,13 @@
 package com.metrostate.ics499.ers.web;
 
 import com.metrostate.ics499.ers.Application;
-import com.metrostate.ics499.ers.DBAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.metrostate.ics499.ers.Location;
 import org.springframework.ui.Model;
 import com.metrostate.ics499.ers.Animal;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,10 +22,24 @@ public class SelectLocAnimalsController {
         return "selectlocanimals";
     }
 
+    @ModelAttribute("locOptions")
+    public List<String> getOptions() {
+        ArrayList<String> options = new ArrayList<>();
+        for (Location loc : Application.getMasterList().getAllLocations()) {
+            options.add(loc.getName());
+        }
+        return options;
+    }
+
     @RequestMapping(path = "/selectLoc", method = RequestMethod.POST)
-    public String contactFormSubmission(Model model, String locID){
-        List<Animal> animals = Application.getMasterList().getLocation(Integer.parseInt(locID)).getAnimals();
-        model.addAttribute("animals", animals);
-        return "selectlocanimals";
+    public String formSub(String locID, Model model){
+        for (Location loc : Application.getMasterList().getAllLocations()) {
+            if (loc.getName().equalsIgnoreCase(locID)) {
+                List<Animal> animals = Application.getMasterList().getLocation(loc.getId()).getAnimals();
+                model.addAttribute("animals", animals);
+                return "selectlocanimals";
+            }
+        }
+        return "redirect:/error.html";
     }
 }
