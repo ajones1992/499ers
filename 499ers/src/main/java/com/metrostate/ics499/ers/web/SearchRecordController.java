f880ecc9f1bcd18703074764234b546bb35821f6
KiyokoNee <k.c.gearing@gmail.com>
2024-04-23 07:53:09 -0500

    updated search record functionality to be more practical

--- 499ers/src/main/java/com/metrostate/ics499/ers/web/SearchRecordController.java
+++ 499ers/src/main/java/com/metrostate/ics499/ers/web/SearchRecordController.java
@@ -2,6 +2,7 @@ package com.metrostate.ics499.ers.web;
 
 import com.metrostate.ics499.ers.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.metrostate.ics499.ers.Record;
@@ -16,65 +17,39 @@ import org.springframework.ui.Model;
 @Controller
 public class SearchRecordController {
 
-    /**
-     * Maps /searchAnimal to the searchrecord.html web page, while also
-     * providing the Form for searching to the page.
-     *
-     * @param model the form to be given to the search value
-     * @return the mapping?
-     */
     @RequestMapping(value = "/searchrecord", method = RequestMethod.GET)
-    public String getSearchAnimal(Model model) {
-        model.addAttribute("searchValue", new FormChoice());
+    public String getSelAnimalRecords(Model model) {
+
+        model.addAttribute("aniChoice", new FormChoice());
         return "searchrecord";
     }
 
-    /**
-     * Posts to the searchrecord.html webpage the results of a given search.
-     *
-     * @param formData the information from the search bar form
-     * @param searchValue the search bar from
-     * @param model the list of animals to be dynamically added to the webpage
-     * @return the mapping
-     */
-    @PostMapping("/searchforrecord")
-    public String searchForValue(@RequestBody MultiValueMap<String, String> formData,
-                                 @ModelAttribute("searchValue") FormChoice searchValue, Model model) {
-        List<Record> results = searchRecord(formData);
-        if (results == null)  return "redirect:/error.html";
-        model.addAttribute("records", results);
-        return "searchrecord";
+    // This creates a model attribute that is a list of animal names
+    // This is what the dropdown is populated with
+    @ModelAttribute("aniOptions")
+    public List<FormSelection> getOptions() {
+        FormSelection temp;
+        ArrayList<FormSelection> options = new ArrayList<>();
+        for (Animal ani : Application.getMasterList().getAllAnimals()) {
+            temp = new FormSelection();
+            temp.setChoice(ani.getName());
+            temp.setChoiceID(ani.getId());
+            options.add(temp);
+        }
+        return options;
     }
 
-    /**
-     * Returns the name entered into the search bar
-     *
-     * @param obj a String representing what the user entered
-     * @return the animal name being searched for
-     */
-    public String getName(Object obj){
-        return removeBrackets(String.valueOf(obj));
-    }
-
-    /**
-     * Removes unnecessary brackets from a search value.
-     *
-     * @param str the search value
-     * @return the search value without brackets
-     */
-    public String removeBrackets(String str){
-        return str.replace("[", "").replace("]", "");
-    }
-
-    /**
-     * Returns a list of records with the provided name in the database.
-     *
-     * @param values a map of search values, currently only contianing a name
-     * @return a list of records attached to a given name found in values
-     */
-    public List<Record> searchRecord(MultiValueMap values) {
-        String name = getName(values.get("name"));
-        List<Animal> query = DBAdapter.queryAnimal("Animal_Name", name);
-        return query.size() > 0? query.get(0).getRecords() : null;
+    // This method displays animals based on the location selected
+    @RequestMapping(value = "/selectAni", method = RequestMethod.POST)
+    public String formSub(@ModelAttribute("aniChoice") FormChoice locChoice, Model model){
+        for (Animal ani : Application.getMasterList().getAllAnimals()) {
+            // Check if the names match for this location and the selected location
+            if (ani.getId() == Integer.parseInt(locChoice.getChoice())) {
+                // Add the list of animals to be displayed as an attribute
+                model.addAttribute("records", ani.getRecords());
+                return "searchrecord";
+            }
+        }
+        return "redirect:/error.html";
     }
 }
